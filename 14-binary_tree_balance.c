d879c6b43b754a873214603511ec2cbb194d67d1
jbn179 <9772@holbertonstudents.com>
2024-12-10 11:24:17 +0100

    add task 17

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
+ * binary_tree_balance - function that measures the balance factor
+ * of a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves
  * Description: function that measures the balance factor of a binary tree
  * Return: If tree is NULL, the function must return 0
